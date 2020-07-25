c9761b9a0989ad5ec75923d9c4318dbc6583ad10
AverageAlien <37085788+AverageAlien@users.noreply.github.com>
2020-07-25 21:57:08 +0300

    added some stuff

---
+++ client/src/app/materials.module.ts
@@ -0,0 +1,56 @@
+import {NgModule} from '@angular/core';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatTableModule} from '@angular/material/table';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatNativeDateModule} from '@angular/material/core';
+
+
+@NgModule({
+  imports: [],
+  exports: [
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTabsModule,
+    MatCardModule,
+    MatStepperModule,
+    MatInputModule,
+    MatDialogModule,
+    MatProgressBarModule,
+    MatSelectModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatMenuModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule,
+    MatBadgeModule,
+    MatExpansionModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ]
+})
+export class MaterialsModule {
+}
