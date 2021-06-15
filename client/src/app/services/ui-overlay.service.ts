4b82dcfe66699c696761494039d7f81f2e1176d3
AverageAlien <37085788+AverageAlien@users.noreply.github.com>
2021-06-15 21:59:13 +0300

    added express-jwt package

--- client/src/app/services/ui-overlay.service.ts
+++ client/src/app/services/ui-overlay.service.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Armor } from '../gameplay/items/armor';
 import { Item } from '../gameplay/items/baseItem';
-import { ItemType } from '../gameplay/items/itemEnums';
-import { Weapon } from '../gameplay/items/weapon';
-import { InventoryItem } from '../models/inventoryItem.model';
 import { ClientLevel } from '../scenes/clientLevel';
 import { NetworkingService } from './networking.service';
 
